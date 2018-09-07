96237764ea1d4ee435db3e80a5ce081ce38b92b6
vikram <vikram2948@gmail.com>
2018-09-08 00:21:01 +0530

    add Application java source

---
+++ src/main/java/f1/Application.java
@@ -0,0 +1,14 @@
+package f1.f2.f3;
+
+public class Application {
+    
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+	Application app = new Application();
+    }
+}
