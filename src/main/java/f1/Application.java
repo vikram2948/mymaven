068d856f1988d5a253fe5166cc5f5e43303d410a
vikram <vikram2948@gmail.com>
2018-09-10 23:44:52 +0530

    changing compiler plug in to java source

--- src/main/java/f1/Application.java
+++ src/main/java/f1/Application.java
@@ -1,14 +1,28 @@
 package f1.f2.f3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+	
+	public void greet() {
+       List<String> greetings = new ArrayList<>();
+       greetings.add("hello");
+	   
+       for (String greeting : greetings) {
+       System.out.println("Greeting: " + greeting);
+       }
+    }
     
     public Application() {
         System.out.println ("Inside Application");
+		
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
-    }
+	    Application app = new Application();
+        app.greet();
+   }
 }
